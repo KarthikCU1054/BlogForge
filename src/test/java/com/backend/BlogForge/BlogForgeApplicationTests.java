13b569a185cd3c63980f86c10a5db054483c8334
Karthik Dammu <karthikcu1054@gmail.com>
2025-09-29 10:47:41 -0400

    Application Setup
    Added front end and all the necessary backend stuff.

---
+++ src/test/java/com/backend/BlogForge/BlogForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.BlogForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
